ef6c58218f92e6a453d2d4a3824d400efdbaf0dd
Jacky Eastmoon <jacky_eastmoon@hotmail.com>
2024-02-18 00:20:31 +0800

    feat: dynamic load web component

--- app/mff/home/src/index.js
+++ app/mff/home/src/index.js
@@ -1,1 +1,45 @@
 console.log("Hello World!");
+
+function loadJSON(filename, callback) {
+    var xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+    xobj.open('GET', filename, true);
+    xobj.onreadystatechange = function () {
+          if (xobj.readyState == 4 && xobj.status == "200") {
+            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            callback(xobj.responseText);
+          }
+    };
+    xobj.send(null);
+}
+
+function webcom(path) {
+    let root = document.querySelector("head");
+    loadJSON(`${path}/asset-manifest.json`, ( restext ) => {
+        var res = JSON.parse(restext)
+        if ( res["entrypoints"] !== undefined ) {
+            res["entrypoints"].forEach((item, i) => {
+                if ( item.endsWith(".js") ) {
+                    console.log(`<script src=${path}/${item} type='module'></script>`)
+                    let elm = document.createElement("script");
+                    elm.src = `${path}/${item}`;
+                    elm.type = 'module';
+                    root.appendChild(elm);
+                }
+                if ( item.endsWith(".css") ) {
+                    console.log(`<link href="${path}/${item}" rel="stylesheet">`)
+                    let elm = document.createElement("link");
+                    elm.href = `${path}/${item}`;
+                    elm.rel = 'stylesheet';
+                    root.appendChild(elm);
+
+                }
+            });
+            console.log(root);
+        }
+    })
+}
+
+webcom('./assets/vue-app');
+webcom('./assets/react-app');
+webcom('./assets/angular-app');
